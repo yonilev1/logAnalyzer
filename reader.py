544b72f2f3f4b2cf6418571e29197801831d7973
Yoni <yonilev2021@gmail.com>
2026-02-12 19:56:43 +0200

    change read_log_file to be a generator and genarate line by line

--- reader.py
+++ reader.py
@@ -1,11 +1,15 @@
 import csv
 import pathlib
 
-def read_log_file(log_file_path):
+"""def read_log_file(log_file_path):
     row_list = []
     with open(log_file_path, 'r') as log_file:
         log_reader = csv.reader(log_file, delimiter=',')
         for row in log_reader:
             row_list.append(row)
-    return row_list
+    return row_list"""
 
+def read_log_file(log_file_path):
+    with open(log_file_path, 'r') as f:
+        for line in f:
+            yield line.strip().split(',')
