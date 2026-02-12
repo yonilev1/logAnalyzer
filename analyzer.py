4d6ee55a6149eacd47c6672c754458ccb0e68dae
Yoni <yonilev2021@gmail.com>
2026-02-12 11:32:25 +0200

    fixed functions that wornt under the orders and fixed some logic

--- analyzer.py
+++ analyzer.py
@@ -1,19 +1,10 @@
 import checks
-import csv
-import pathlib
-import reader
-from collections import Counter
-from datetime import datetime
+from collections import Counter, defaultdict
 import config
 
 def is_packet_normal(list_of_rows):
-    packet_abnormality = []
     large_rows = checks.extract_rows_over_5000_bytes(list_of_rows)
-    for row in list_of_rows:
-        if row in large_rows:
-            packet_abnormality.append("LARGE")
-        else:
-            packet_abnormality.append("NORMAL")
+    packet_abnormality = ["LARGE" if row in large_rows else "NORMAL" for row in list_of_rows]
     return packet_abnormality
 
 
@@ -28,44 +19,30 @@ def port_number_to_name(list_of_rows):
 
 
 def suspicion_recognition_for_ip(list_of_rows):
-    external_ips = checks.extract_external_ip(list_of_rows)
-    ports = port_number_to_name(list_of_rows)
-    size_of_packet = is_packet_normal(list_of_rows)
-    suspicion = {}
+    external_ips = set(checks.extract_external_ip(list_of_rows))
+    size_tags = is_packet_normal(list_of_rows)
+    suspicion = defaultdict(set)
     for index, row in enumerate(list_of_rows):
-        sus = []
+        sus = set()
         if row[1] in external_ips:
-            sus.append("EXTERNAL_IP")
+            sus.add("EXTERNAL_IP")
 
-        if size_of_packet[index] == "LARGE":
-            sus.append("LARGE_PACKET")
+        if size_tags[index] == "LARGE":
+            sus.add("LARGE_PACKET")
 
         if row[3] in config.SENSITIVE_PORT:
-            sus.append("SENSITIVE_PORT")
+            sus.add("SENSITIVE_PORT")
 
-        dt_object = datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S")
+        hour = int(row[0][11:13])
+        if 0 <= hour < 6:
+            sus.add("NIGHT_ACTIVITY")
 
-        if 0 <= dt_object.hour < 6:
-            sus.append("NIGHT_ACTIVITY")
-
-        if row[1] not in suspicion.keys():
-            suspicion[row[1]] = sus
-        else:
-            if len(suspicion[row[1]]) < len(sus):
-                suspicion[row[1]] = sus
-            elif len(suspicion[row[1]]) == len(sus):
-                if suspicion[row[1]] != sus:
-                    sus += suspicion[row[1]]
-                    sus = set(sus)
-                    suspicion[row[1]] = list(sus)
+        suspicion[row[1]].update(sus)
     return suspicion
 
 
 def get_all_ips_with_at_list_2_suspicion2(sus_map):
-    at_list_2_suspicion2 = {}
-    for key, value in sus_map.items():
-        if len(value) >= 2:
-            at_list_2_suspicion2[key] = value
+    at_list_2_suspicion2 = {key : value for key, value in sus_map.items() if len(value) >= 2}
     return at_list_2_suspicion2
 
 
