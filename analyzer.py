c21222ea2c3649801a1b577cfbf61b5cafeb3527
Yoni <yonilev2021@gmail.com>
2026-02-12 10:16:47 +0200

    add suspicion_recognition_for_ip that maps each ip to a list of suspiciouns

--- analyzer.py
+++ analyzer.py
@@ -3,6 +3,8 @@ import csv
 import pathlib
 import reader
 from collections import Counter
+from datetime import datetime
+import config
 
 def is_packet_normal(list_of_rows):
     packet_abnormality = []
@@ -21,10 +23,43 @@ def count_ip_references(list_of_rows):
     return count
 
 
-def port_number_to_name(log_list):
-    port_number_name = {row[3] : row[4] for row in log_list}
+def port_number_to_name(list_of_rows):
+    port_number_name = {row[3] : row[4] for row in list_of_rows}
     return port_number_name
 
+
+def suspicion_recognition_for_ip(list_of_rows):
+    external_ips = checks.extract_external_ip(list_of_rows)
+    ports = port_number_to_name(list_of_rows)
+    size_of_packet = is_packet_normal(list_of_rows)
+    suspicion = {}
+    for index, row in enumerate(list_of_rows):
+        sus = []
+        if row[1] in external_ips:
+            sus.append("EXTERNAL_IP")
+
+        if size_of_packet[index] == "LARGE":
+            sus.append("LARGE_PACKET")
+
+        if row[3] in config.SENSITIVE_PORT:
+            sus.append("SENSITIVE_PORT")
+
+        dt_object = datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S")
+
+        if 0 <= dt_object.hour < 6:
+            sus.append("NIGHT_ACTIVITY")
+
+        if row[1] not in suspicion.keys():
+            suspicion[row[1]] = sus
+        else:
+            if len(suspicion[row[1]]) < len(sus):
+                suspicion[row[1]] = sus
+            elif len(suspicion[row[1]]) == len(sus):
+                if suspicion[row[1]] != sus:
+                    sus += suspicion[row[1]]
+                    sus = set(sus)
+                    suspicion[row[1]] = list(sus)
+    return suspicion
 
 
 
--- reader.py
+++ reader.py
@@ -8,3 +8,4 @@ def read_log_file(log_file_path):
         for row in log_reader:
             row_list.append(row)
     return row_list
+
