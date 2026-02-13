c7e5fe3736cc47d5b9b4ae6f67baf53f2f749591
Yoni <yonilev2021@gmail.com>
2026-02-13 11:17:52 +0200

    fixed save_report to work with utf-8, and added main functionality to run the program

--- reporter.py
+++ reporter.py
@@ -1,27 +1,43 @@
 import analyzer
 
+
 def generate_report(suspicious_dict):
     three_sus = {}
+    other_sus = {}
+
     for key, value in suspicious_dict.items():
         if len(value) > 2:
             three_sus[key] = value
+        else:
+            other_sus[key] = value
 
+    report = "==============================\n"
+    report += "      דוח תעבורה חשודה\n"
+    report += "==============================\n\n"
 
-    print("General Statistics:")
-    print(f"-Number of lines: {analyzer.number_of_lines}")
-    print(f"-Number of suspicious lines: {analyzer.number_of_suspicious_lines}")
-    print(f"-Number of EXTERNAL_IP events: {analyzer.suspicion_counts['EXTERNAL_IP']}")
-    print(f"-Number of LARGE_PACKET events: {analyzer.suspicion_counts['LARGE_PACKET']}")
-    print(f"-Number of SENSITIVE_PORT events: {analyzer.suspicion_counts['SENSITIVE_PORT']}")
-    print(f"-Number of NIGHT_ACTIVITY events: {analyzer.suspicion_counts['NIGHT_ACTIVITY']}")
+    report += "General Statistics:\n"
+    report += f"- Number of lines: {analyzer.number_of_lines}\n"
+    report += f"- Number of suspicious lines: {analyzer.number_of_suspicious_lines}\n"
+    report += f"- EXTERNAL_IP events: {analyzer.suspicion_counts['EXTERNAL_IP']}\n"
+    report += f"- LARGE_PACKET events: {analyzer.suspicion_counts['LARGE_PACKET']}\n"
+    report += f"- SENSITIVE_PORT events: {analyzer.suspicion_counts['SENSITIVE_PORT']}\n"
+    report += f"- NIGHT_ACTIVITY events: {analyzer.suspicion_counts['NIGHT_ACTIVITY']}\n\n"
 
-    print("IPs with high level risk (3+ suspicions):")
+    report += "IPs with high level risk (3+ suspicions):\n"
     for key, value in three_sus.items():
-        print(f"{key}: {value}")
+        sus_str = ", ".join(value)
+        report += f"- {key}: {sus_str}\n"
+
+    report += "\nAdditional suspicious IPs:\n"
+    for key, value in other_sus.items():
+        sus_str = ", ".join(value)
+        report += f"- {key}: {sus_str}\n"
+
+    return report
 
 
-def save_report(report):
-    with open("log_report.txt", "a") as f:
+def save_report(report, file_path):
+    with open(file_path, "a", encoding="utf-8") as f:
         f.write(report)
         f.write("\n")
 
